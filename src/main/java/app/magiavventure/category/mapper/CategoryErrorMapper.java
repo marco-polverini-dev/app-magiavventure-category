a98b04aec9383cb907f1143f26af7339f42abf65
Marco Polverini <marco.polverini.dev@gmail.com>
2023-12-09 21:57:57 +0100

    refactor: removed service from code error

--- src/main/java/app/magiavventure/category/mapper/CategoryErrorMapper.java
+++ src/main/java/app/magiavventure/category/mapper/CategoryErrorMapper.java
@@ -3,18 +3,11 @@ package app.magiavventure.category.mapper;
 import app.magiavventure.category.configuration.CategoryProperties.ErrorProperties.ErrorMessage;
 import app.magiavventure.category.error.handler.HttpError;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
 import org.mapstruct.ReportingPolicy;
 
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING, unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface CategoryErrorMapper {
-    String SERVICE = "category";
-
-    @Mapping(target = "code", expression = "java(mapCode(errorMessage))")
     HttpError map(ErrorMessage errorMessage);
 
-    default String mapCode(ErrorMessage errorMessage) {
-        return SERVICE.concat(":").concat(errorMessage.getCode());
-    }
 }
